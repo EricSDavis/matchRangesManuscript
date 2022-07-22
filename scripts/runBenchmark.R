4cdbaf1ab3b9ce34d634aa07e767fa5e682ddfdc
Eric Davis <ericscottdavis@outlook.com>
2022-07-22 10:06:16 -0400

    Increase the number of reps; set seed internally to ensure the same sets are pulled each time.

--- scripts/runBenchmark.R
+++ scripts/runBenchmark.R
@@ -10,43 +10,65 @@ source("scripts/makeExampleData.R")
 ## Set the points for sample size
 points <- 10^(seq(4, 6, by = 0.25))
 
-## Set seed for reproducibility
-set.seed(123)
+## Apply benchmarking for each sample size in points
 results <- 
   lapply(points, \(n) {
     
     message(n)
-    ## Generate dataset
-    df <- makeExampleData(total = n, ff = 0.05)
     
     ## Run microbenchmark
     microbenchmark(
-      nearest = matchRanges(focal = df[df$feature1,],
-                            pool = df[!df$feature1,],
-                            covar = ~feature2 + feature3,
-                            method = "nearest",
-                            replace = TRUE),
-      stratified_with_replacement = matchRanges(focal = df[df$feature1,],
-                                                pool = df[!df$feature1,],
-                                                covar = ~feature2 + feature3,
-                                                method = "stratified",
-                                                replace = TRUE),
-      stratified_without_replacement = matchRanges(focal = df[df$feature1,],
-                                                   pool = df[!df$feature1,],
-                                                   covar = ~feature2 + feature3,
-                                                   method = "stratified",
-                                                   replace = FALSE),
-      rejection_with_replacement = matchRanges(focal = df[df$feature1,],
-                                               pool = df[!df$feature1,],
-                                               covar = ~feature2 + feature3,
-                                               method = "rejection",
-                                               replace = TRUE),
-      rejection_without_replacement = matchRanges(focal = df[df$feature1,],
-                                                  pool = df[!df$feature1,],
-                                                  covar = ~feature2 + feature3,
-                                                  method = "rejection",
-                                                  replace = FALSE),
-      times = 1
+      setup = {
+        ## Generate dataset
+        set.seed(20220722)
+        df <- makeExampleData(total = n, ff = 0.05)
+      },
+      nearest = {
+        message("nearest")
+        set.seed(20220722)
+        matchRanges(focal = df[df$feature1,],
+                    pool = df[!df$feature1,],
+                    covar = ~feature2 + feature3,
+                    method = "nearest",
+                    replace = TRUE)
+      },
+      stratified_with_replacement = {
+        message("stratified_with_replacement")
+        set.seed(20220722)
+        matchRanges(focal = df[df$feature1,],
+                    pool = df[!df$feature1,],
+                    covar = ~feature2,
+                    method = "stratified",
+                    replace = TRUE)
+      },
+      stratified_without_replacement = {
+        message("stratified_without_replacement")
+        set.seed(20220722)
+        matchRanges(focal = df[df$feature1,],
+                    pool = df[!df$feature1,],
+                    covar = ~feature2,
+                    method = "stratified",
+                    replace = FALSE)
+      },
+      rejection_with_replacement = {
+        message("rejection_with_replacement")
+        set.seed(20220722)
+        matchRanges(focal = df[df$feature1,],
+                    pool = df[!df$feature1,],
+                    covar = ~feature2,
+                    method = "rejection",
+                    replace = TRUE)
+      },
+      rejection_without_replacement = {
+        message("rejection_without_replacement")
+        set.seed(20220722)
+        matchRanges(focal = df[df$feature1,],
+                    pool = df[!df$feature1,],
+                    covar = ~feature2,
+                    method = "rejection",
+                    replace = FALSE)
+      },
+      times = 20L
     )
     
   })
@@ -56,4 +78,3 @@ results <- setNames(results, points)
 
 ## Save benchmark results
 saveRDS(object = results, file = "data/benchmarkResults.rds")
-
